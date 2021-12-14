344412e846584c90836dcd549173c3440a0d0a6d
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-14 14:39:22 +0000

    Add status to show if a post is in draft or published

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create a constant tuple to make sure if a post is in draft or published
+STATUS = ((0, 'Draft'), (1, 'Published'))
