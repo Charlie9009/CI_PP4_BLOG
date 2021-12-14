4c792a3e4a4f2ab1603c42428e995ce31a1002bb
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-14 18:54:47 +0000

    VIEWS.PY: Add a PostList class to render the html for the user

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """
+    Using ListView to render to the index.html.
+    Also filtering so only published posts show up
+    """
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
