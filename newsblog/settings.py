de2bd57154b845af5a8b01b092d43771b401a919
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-14 09:39:23 +0000

    Add imports from env and add 'SECRET_KEY' to settings.py

--- newsblog/settings.py
+++ newsblog/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7gmo5qqv)j*j@(#t$bdrd5whl1)mtghq04-bbid3#hooi%=!+d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
