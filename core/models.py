28cd9062ef4f8a31c3d193fec35feaca40698a0c
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-14 15:15:46 +0000

    MODELS.PY: Add post class to post news

--- core/models.py
+++ core/models.py
@@ -3,3 +3,20 @@ from django.contrib.auth.models import User
 
 # Create a constant tuple to make sure if a post is in draft or published
 STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images')
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
