b7fe2a3618d4dec7e6c28ee24f8ead70426211f2
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-14 16:52:09 +0000

    ADMIN.PY: Add a function to make it easier to find posts

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    Create class and variables to make reading, finding, searching
+    and filtering posts easier
+    """
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
