592923fc1665992d353adb70a08fbfa04f8427b3
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-15 19:24:50 +0000

    SETTINGS.PY: Add auth in settings.py at installed_apps

--- newsblog/settings.py
+++ newsblog/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'core',
     'crispy_forms',
 ]
