50842681f6171cec19e1f78f664c41883d2f3673
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-15 09:23:20 +0000

    MODELS.PY: Add Comment model to create comment function

--- core/models.py
+++ core/models.py
@@ -9,8 +9,8 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images', blank=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now= True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -20,3 +20,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    """
+    Create a comment model to be able to comment on posts with names and text.
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
