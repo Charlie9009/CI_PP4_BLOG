5527ee72efe9c6e641b020fbe85082aa316ae7cb
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-15 13:54:27 +0000

    VIEWS.PY: Changed template_name in post_detail function and added PostDetail class

--- core/views.py
+++ core/views.py
@@ -16,8 +16,13 @@ class PostList(generic.ListView):
     template_name = 'index.html'
 
 
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
 def post_detail(request, slug):
-    template_name = 'index.html'
+    template_name = 'post_detail.html'
     post = get_object_or_404(Post, slug=slug)
     comments = post.comments.filter(active=True)
     new_comment = None
