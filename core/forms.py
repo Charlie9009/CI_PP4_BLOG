5f5c8405869a67b612786fa77b1fb649354e401e
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-15 09:58:24 +0000

    FORMS.PY: Add comments for forms.py file

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,14 @@
+"""
+Import from django and from models module
+"""
 from django import forms
 from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    """
+    Create class to generate form for Comments.
+    """
     class Meta:
         model = Comment
         fields = ('name', 'body')
