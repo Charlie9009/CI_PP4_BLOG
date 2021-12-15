8839e01a0505c066f5549ea288e860c2d542c848
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-15 20:08:15 +0000

    URLS.PY: Add paths for allauth.   SETTINGS.PY: Add allauth to installed_apps and add login and logut urls

--- newsblog/settings.py
+++ newsblog/settings.py
@@ -43,9 +43,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'core',
     'crispy_forms',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
