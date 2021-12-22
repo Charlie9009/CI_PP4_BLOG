8e4f918ffab6d57e3376649a41bfc24ec6afbc47
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-22 18:48:17 +0000

    URLS.PY: Add new url path

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='index'),
     path('search/', views.SearchResultsView.as_view(), name='search_results'),
     path('question/', views.QuestionView.as_view(), name='question'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
 ]
