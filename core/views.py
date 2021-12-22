3d97ed4dc28645a53ef70acfb14c8f8ecd1bf6f9
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-22 18:47:16 +0000

    VIEWS.PY: Remake class PostDetail

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 """
 Import from django, models module and from forms module.
 """
+from django.urls import reverse_lazy
 from django.views import generic, View
+from django.views.generic.edit import UpdateView, DeleteView
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-from .models import Post, Question
+from .models import Post, Question, Comment
 from .forms import CommentForm
 
 
@@ -19,61 +21,39 @@ class PostList(generic.ListView):
 
 class PostDetail(View):
     """
-    This class 'PostDetail' is taken from Code Institutes school project
+    This class 'PostDetail' was first taken from Code Institutes school project
     'DjangoBlog'
-    and altered slightly for this project.
-    It is added so that a user can comment on the page
+    and then reworked a bit for this project.
+    It is added so that a user can get the posts displayed and be able
+    to make a comment
     """
-    def get(self, request, slug, *args, **kwargs):
-        """
-        Create function to filter the query of active posts and comments
-        and render them to the user.
-        """
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(active=True).order_by('-created_on')
+    def get(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        comment_form = CommentForm()
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+        context = {
+            'post': post,
+            'comments': comments,
+            'comment-form': comment_form,
+        }
+        return render(request, 'post_detail.html', context)
 
-        return render(
-            request,
-            'post_detail.html',
-            {
-                'post': post,
-                'comments': comments,
-                'commented': False,
-                'comment_form': CommentForm()
-            },
-        )
-
-    def post(self, request, slug, *args, **kwargs):
-        """
-        Create function to filter the query of active posts and comments
-        Also if a user commented successfully on the page that comment is
-        saved to the site
-        """
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(active=True).order_by('-created_on')
-
-        comment_form = CommentForm(data=request.POST)
-
+    def post(self, request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
-            comment_form.instance.name = request.user
             comment = comment_form.save(commit=False)
+            comment.author = request.user
             comment.post = post
             comment.save()
-        else:
-            comment_form = CommentForm()
-
-        return render(
-            request,
-            'post_detail.html',
-            {
-                'post': post,
-                'comments': comments,
-                'commented': True,
-                'comment_form': CommentForm()
-            },
-        )
+        
+        comments = Comment.objects.filter(post=post).order_by('-created_on')
+        context = {
+            'post': post,
+            'comments': comments,
+            'comment-form': comment_form,
+        }
+        return render(request, 'post_detail.html', context)
 
 
 class SearchResultsView(generic.ListView):
