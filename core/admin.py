388b6e0f6acaa2932ec7bb676c254669156e524f
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-22 18:28:01 +0000

    ADMIN.PY Remove slug from list_display and prepopulated_fields

--- core/admin.py
+++ core/admin.py
@@ -10,10 +10,9 @@ class PostAdmin(admin.ModelAdmin):
     Create class and variables to make reading, finding, searching
     and filtering posts easier
     """
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'status', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Post, PostAdmin)
