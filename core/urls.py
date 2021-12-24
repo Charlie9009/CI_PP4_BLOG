e3b8a52b0404217ff8b48fa5f8621cd0bfa6583b
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-24 12:43:10 +0000

    URLS.PY: Add path for PostUpdate

--- core/urls.py
+++ core/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('question/', views.QuestionView.as_view(), name='question'),
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('post/new/', views.PostCreate.as_view(), name='post_create'),
+    path('post/<int:pk>/update/', views.PostUpdate.as_view(), name='post-update'),
 ]
