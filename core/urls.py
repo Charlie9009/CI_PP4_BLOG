6b838c3bc3d248395295f14e8e7d2482ef2682fd
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-24 12:15:44 +0000

    URLS.PY: Write correct class name in path

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='index'),
     path('search/', views.SearchResultsView.as_view(), name='search_results'),
     path('question/', views.QuestionView.as_view(), name='question'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
 ]
