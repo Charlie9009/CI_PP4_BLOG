4b471434340b1520d9b762e636ac3dc8c103f29c
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-24 12:14:19 +0000

    VIEWS.PY: Change import and update PostDetail

--- core/views.py
+++ core/views.py
@@ -2,14 +2,13 @@
 Import from django, models module and from forms module.
 """
 from django.views import generic, View
-from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from .models import Post, Question, Comment
 from .forms import CommentForm
 
 
-class PostList(ListView):
+class PostList(generic.ListView):
     """
     Using ListView to render to the index.html.
     Also filtering so only published posts show up
@@ -20,7 +19,7 @@ class PostList(ListView):
     ordering = ['-created_on']
 
 
-class PostDetail(View):
+class PostDetail(generic.DetailView):
     """
     This class 'PostDetail' was first taken from Code Institutes school project
     'DjangoBlog'
@@ -28,6 +27,9 @@ class PostDetail(View):
     It is added so that a user can get the posts displayed and be able
     to make a comment
     """
+    model = Post
+    template_name = 'post_detail.html'
+    
     def get(self, request, pk, *args, **kwargs):
         post = Post.objects.get(pk=pk)
         comment_form = CommentForm()
