aa8dd37f5e0e9742275e8dc6c4966a79ddb9294f
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-24 12:47:30 +0000

    URLS.PY: Add path for PostDelete and change name for PostUpdate

--- core/urls.py
+++ core/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('question/', views.QuestionView.as_view(), name='question'),
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('post/new/', views.PostCreate.as_view(), name='post_create'),
-    path('post/<int:pk>/update/', views.PostUpdate.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', views.PostUpdate.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDelete.as_view(), name='post_delete'),
 ]
