0e4c6819c94a5691fe5c876a26208e5b5fc59b1b
Charlie9009 <charlie.gustafsson90@gmail.com>
2021-12-26 23:19:57 +0000

    FORM.JS: Change code for js

--- static/form.js
+++ static/form.js
@@ -1,5 +1,11 @@
-let closeModal = document.getElementById('msg');
-let bootstrap = document.getElementsByClassName('alert-success');
-closeModal.addEventListener('click', () => {
-    bootstrap.close();
-});
+setTimeout(function() {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3000);
+
+document.getElementById('hello').addEventListener("click", helloModal);
+
+function helloModal() {
+  alert ("Hello there how'd you find me?!?");
+}
